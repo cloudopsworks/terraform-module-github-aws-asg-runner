3a06769a9173e9bd8ce9a3ccd6aa94fa38c9894a
Cristian Beraha <47121397+berahac@users.noreply.github.com>
2025-09-02 18:19:21 -0300

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,41 @@
+##
+# (c) 2021-2025
+#     Cloud Ops Works LLC - https://cloudops.works/
+#     Find us on:
+#       GitHub: https://github.com/cloudopsworks
+#       WebSite: https://cloudops.works
+#     Distributed Under Apache v2.0 License
+#
+
+# Establish this is a HUB or spoke configuration
+variable "is_hub" {
+  description = "Is this a hub or spoke configuration?"
+  type        = bool
+  default     = false
+}
+
+variable "spoke_def" {
+  description = "Spoke ID Number, must be a 3 digit number"
+  type        = string
+  default     = "001"
+  validation {
+    condition     = (length(var.spoke_def) == 3) && tonumber(var.spoke_def) != null
+    error_message = "The spoke_def must be a 3 digit number as string."
+  }
+}
+
+variable "org" {
+  description = "Organization details"
+  type = object({
+    organization_name = string
+    organization_unit = string
+    environment_type  = string
+    environment_name  = string
+  })
+}
+
+variable "extra_tags" {
+  description = "Extra tags to add to the resources"
+  type        = map(string)
+  default     = {}
+}
